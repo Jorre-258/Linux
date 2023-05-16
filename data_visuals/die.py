b50150bbd419b48afcabfd7cbd4872aac43cbdd2
jorre18 <delcroixjorre@gmail.com>
2023-05-16 21:32:53 +0200

    data_visuals

--- data_visuals/die.py
+++ data_visuals/die.py
@@ -3,5 +3,10 @@ from random import randint
 class Die():
     """A class representing a die"""
 
-    def __init__(selfself, num_sides = 6):
-        """Assume a six side"""
+    def __init__(self, num_sides = 6):
+        """Assume a six sided die."""
+        self.num_sides = num_sides
+
+    def roll(self):
+        """Return a random value between 1 and number of sides"""
+        return randint(1, self.num_sides)
